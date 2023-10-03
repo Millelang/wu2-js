0eb47e5e0272836b2395f6aa207a8fc4e5cdf9c9
Millelang <millelang2005@gmail.com>
2023-10-03 08:58:47 +0200

    operatorer

--- practice.js
+++ practice.js
@@ -24,9 +24,24 @@ function varsAndTypes(element) {
   let siffra2 = 5120
   let cool = true
   const max = 12
+  
 }
 
 function operators(element) {
+  let nummer = 325
+  let nummer2 =583
+  let nummer3 =321
+  let nummer4 =134
+  let nummer5 = 215
+  let nummer6 = 632
+  let antal = 6
+  nummer = 5
+  let medelvarde = (nummer+nummer2+nummer3+nummer4+nummer5+nummer6)/antal
+  console.log(medelvarde)
+  if(nummer==nummer2) {
+    console.log('bläh')
+  }
+
   // kod för att visa vad du lärt dig om operatorer
 }
 
